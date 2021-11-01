17a4f9cab029c7164ef3d46a110c0be17a31c547
AviralCoder <aviral.kochgaway@gmail.com>
2021-11-01 11:59:33 +0530

    added montserrat bold

--- functions/loadFonts.js
+++ functions/loadFonts.js
@@ -3,6 +3,7 @@ import * as Font from "expo-font";
 const loadFonts = async () => {
     await Font.loadAsync({
         MontserratRegular: require("../assets/fonts/Montserrat/Montserrat-Regular.ttf"),
+        MontserratBold: require("../assets/fonts/Montserrat/Montserrat-Bold.ttf"),
     });
 };
 
